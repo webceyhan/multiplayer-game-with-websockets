fa0553cfd2a2538318d18a64d16088cf10276225
Evren Ceyhan <evren@ceyhan.io>
2022-01-26 21:46:48 +0300

    eslint browser enabled

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
     env: {
-      es2021: true,
-      node: true,
+        es2021: true,
+        browser: true,
+        node: true,
     },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended"
-    ],
-    parser: "@typescript-eslint/parser",
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    plugins: ["@typescript-eslint"],
+    plugins: ['@typescript-eslint'],
     rules: {},
-  };
+};
