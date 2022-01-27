31e9b6bc2cf013a1f8d73b198e9151a963cbe845
Evren Ceyhan <evren@ceyhan.io>
2022-01-28 00:57:55 +0300

    vue migration

--- public/app.js
+++ public/app.js
@@ -1,164 +1,75 @@
-// VUE APP /////////////////////////////////////////////////////////////////////////////////////////
-
 // eslint-disable-next-line no-undef
-Vue.createApp({}).mount('#app');
+Vue.createApp({
+    data() {
+        return {
+            game: {},
+            player: {},
+            ws: null,
+        };
+    },
+    computed: {
+        ready() {
+            return !!this.game.id;
+        },
+        started() {
+            return this.players.findIndex((p) => p.id === this.player.id) > -1;
+        },
+        players() {
+            return this.game.players || [];
+        },
+        balls() {
+            const amount = 20;
+            const state = this.game.state || {};
 
-/**
- * define constants
- */
-let gameId = null;
-let playerId = null;
-let playerColor = null;
+            return Array(amount)
+                .fill(null)
+                .map((v, i) => state[i]);
+        },
+    },
+    methods: {
+        sendEvent(name, payload) {
+            this.ws.send(JSON.stringify({ name, payload }));
+        },
+        onCreate() {
+            this.sendEvent('create');
+        },
+        onJoin() {
+            this.sendEvent('join', { gameId: this.game.id });
+        },
+        onPlay(ballId) {
+            this.sendEvent('play', { gameId: this.game.id, ballId });
+        },
+    },
+    created() {
+        // create new socket connection
+        this.ws = new WebSocket('ws://localhost:9090');
 
-// DOM /////////////////////////////////////////////////////////////////////////////////////////////
+        this.ws.onmessage = (message) => {
+            // parse event name and payload
+            const { name, payload } = JSON.parse(message.data);
 
-/**
- * define elements
- */
-const createBtn = document.getElementById('create-btn');
-const joinBtn = document.getElementById('join-btn');
-const gameIdInput = document.getElementById('game-id');
-const playersDiv = document.getElementById('players');
-const boardDiv = document.getElementById('board');
+            switch (name) {
+                case 'connect':
+                    this.player = payload;
+                    console.log(`client created: ${this.player.id}`);
+                    break;
 
-/**
- * helpers
- */
-const createElement = (tag, text) => {
-    const el = document.createElement(tag);
-    el.textContent = text;
+                case 'create':
+                    this.game = payload;
+                    console.log(`game created" ${this.game.id} `);
+                    break;
 
-    return el;
-};
+                case 'join':
+                    this.game = payload;
+                    console.log(`player joined the game: ${this.game.id}`);
+                    break;
 
-const clearDiv = (div) => {
-    while (div.firstChild) {
-        div.removeChild(div.firstChild);
-    }
-};
-
-const createPlayer = ({ id, name, color }) => {
-    const div = createElement('div');
-
-    div.style.background = color;
-    div.classList.add('list-group-item');
-    div.appendChild(createElement('h6', name));
-    div.appendChild(createElement('small', id));
-
-    return div;
-};
-
-const createBall = (ballId) => {
-    const btn = createElement('button', ballId);
-
-    btn.id = `ball${ballId}`;
-    btn.tag = ballId;
-    btn.classList.add(
-        'btn',
-        'btn-secondary',
-        'border-dark',
-        'flex-fill',
-        'w-25',
-        'p-5'
-    );
-
-    btn.addEventListener('click', (e) => {
-        btn.style.background = playerColor;
-
-        sendMessage('play', { gameId, ballId });
-    });
-
-    return btn;
-};
-
-const initUI = () => {
-    // disable controls
-    joinBtn.disabled = true;
-
-    // clear div containers
-    clearDiv(playersDiv);
-    clearDiv(boardDiv);
-};
-
-/**
- * init events
- */
-createBtn.addEventListener('click', (e) => {
-    sendMessage('create');
-    joinBtn.disabled = false;
-});
-
-joinBtn.addEventListener('click', (e) => {
-    gameId = gameIdInput.value;
-    sendMessage('join', { gameId });
-});
-
-/**
- * init UI
- */
-initUI();
-
-// WEBSOCKET LOGIC /////////////////////////////////////////////////////////////////////////////////
-
-const ws = new WebSocket('ws://localhost:9090');
-
-const sendMessage = (name, payload) => {
-    ws.send(JSON.stringify({ name, payload }));
-};
-
-ws.onmessage = (message) => {
-    const event = JSON.parse(message.data);
-
-    switch (event.name) {
-        case 'connect':
-            playerId = event.payload.id;
-            console.log(`client id set to ${playerId}`);
-            break;
-
-        case 'create':
-            gameId = event.payload.id;
-            gameIdInput.value = gameId;
-
-            console.log(
-                `game created with id ${gameId} and ${event.payload.balls} balls`
-            );
-            break;
-
-        case 'join': {
-            initUI();
-            const game = event.payload;
-            console.log(game);
-
-            gameId = game.id;
-
-            game.players.forEach((player) => {
-                // set own color
-                if (player.id == playerId) {
-                    playerColor = player.color;
+                case 'play': {
+                    this.game = payload;
+                    console.log('ball state change: ', this.game.state);
+                    break;
                 }
-
-                const playerDiv = createPlayer(player);
-                playersDiv.appendChild(playerDiv);
-            });
-
-            // fill the board with balls
-            for (let i = 0; i < game.balls; i++) {
-                const ball = createBall(i + 1);
-                boardDiv.appendChild(ball);
             }
-
-            break;
-        }
-
-        case 'play': {
-            const { state } = event.payload;
-
-            Object.entries(state).forEach(([ballId, color]) => {
-                const ball = document.getElementById('ball' + ballId);
-                ball.style.backgroundColor = color;
-            });
-
-            break;
-        }
-    }
-};
+        };
+    },
+}).mount('#app');
