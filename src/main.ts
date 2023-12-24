38572d014e6b5daa8bbd613c504c6d7d22775079
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-24 22:20:24 +0100

    update server port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createHttpServer, createSocketServer } from './server';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const httpServer = createHttpServer(port);
 const socketServer = createSocketServer(httpServer);
